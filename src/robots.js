87a0810fc3790761f9b070eb2ba91f54b7b6d79a
evidya <Naveen>
2021-01-24 15:52:40 +0530

    files

---
+++ src/robots.js
@@ -0,0 +1,45 @@
+
+export const robots = [
+{
+    id: 1,
+    name: "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 2,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 3,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 4,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 5,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 6,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+},
+{
+    id : 7,
+    name : "John dae",
+    username : "john",
+    email : "johndae@live.com"
+}
+]
